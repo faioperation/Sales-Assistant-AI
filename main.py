de2f5f74bf45e894a5634e95860bc7973363744c
Md Tareq Shah Alam <tareqshah.027@gmail.com>
2026-05-25 12:33:48 +0600

    port change to 8036 from 8000

--- main.py
+++ main.py
@@ -83,4 +83,4 @@ def full_analysis_endpoint(req: ChatRequest):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=False)
+    uvicorn.run(app, host="0.0.0.0", port=8036, reload=False)
